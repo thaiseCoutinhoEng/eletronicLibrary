392e02d32a5933265be0e13c9b34d4791cedde26
Thaise Coutinho <thaisecoutinho.eng@gmail.com>
2023-06-13 17:07:18 -0300

    Autenticacao de usuarios

--- App.js
+++ App.js
@@ -1,35 +1,52 @@
-import React, {Component} from 'react';
-import BottomTabNavigator from './components/BottomTabNavigator';
-import * as Font from "expo-font";
-import { loadAsync } from "expo-font";
+import React, { Component } from "react";
 import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
+import db from "./config";
+import LoginScreen from "./screens/Login";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
 export default class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
+    this.state = {
       fontLoaded: false
     };
   }
-  //funcao assicrona para carregar as fontes
-  async loadFonts(){
+
+  async loadFonts() {
     await Font.loadAsync({
-        Rajdhani_600SemiBold: Rajdhani_600SemiBold
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold
     });
-    this.setState({fontLoaded: true});
+    this.setState({ fontLoaded: true });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.loadFonts();
   }
 
-  render(){
-    const {fontLoaded} = this.state;
-    if(fontLoaded){
-      return <BottomTabNavigator />;
+  render() {
+    const { fontLoaded } = this.state;
+    if (fontLoaded) {
+      return <AppContainer />;
     }
     return null;
   }
-
 }
 
+const AppSwitchNavigator = createSwitchNavigator(
+  {
+    Login: {
+      screen: LoginScreen
+    },
+    BottomTab: {
+      screen: BottomTabNavigator
+    }
+  },
+  {
+    initialRouteName: "Login"
+  }
+);
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
